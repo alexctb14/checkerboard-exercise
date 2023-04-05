dea79d33ca365070009259fcf49b1e9413694e4c
root <root@LAPTOP-T9HP8IPL>
2023-04-05 15:33:09 -0500

    RandomColors

--- script.js
+++ script.js
@@ -9,21 +9,14 @@ container.style.height = '800px';
 
 for (let i = 1; i <= 64; i++) {
     let tile = document.createElement('div');
-    if (Math.floor((i - 1) / 8) % 2 === 0) {
-        if (i % 2 === 1) {
-        tile.style.background = 'black'
-    } else {
-        tile.style.background = 'red'
-    }
-    } else {
-        if (i % 2 === 1) {
-            tile.style.background = 'red'
-        } else {
-            tile.style.background = 'black'
-        }
-    }
     tile.style.width = '12.5%';
     tile.style.height = '12.5%';
+
+    let randomR = Math.floor(Math.random() * 256);
+    let randomG = Math.floor(Math.random() * 256);
+    let randomB = Math.floor(Math.random() * 256);
+
+    tile.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
     container.append(tile);
 }
 
